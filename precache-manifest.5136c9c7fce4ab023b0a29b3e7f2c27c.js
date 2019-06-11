d221dbe3d7c39864d7f5db5867161cbe9effe442
ali <46577578+alinaeemaei@users.noreply.github.com>
2019-06-11 20:03:31 +0430

    Updates

---
+++ precache-manifest.5136c9c7fce4ab023b0a29b3e7f2c27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e455cf47ff282a177160bff82406c3",
+    "url": "/Final-weather/index.html"
+  },
+  {
+    "revision": "b2456acced56b934bacd",
+    "url": "/Final-weather/static/css/main.bcdc7622.chunk.css"
+  },
+  {
+    "revision": "8fd85d4fda5e9775bee8",
+    "url": "/Final-weather/static/js/2.7515ac0a.chunk.js"
+  },
+  {
+    "revision": "b2456acced56b934bacd",
+    "url": "/Final-weather/static/js/main.8a523aba.chunk.js"
+  },
+  {
+    "revision": "edd3c9be4b13d892d4d0",
+    "url": "/Final-weather/static/js/runtime~main.d21ef19e.js"
+  }
+]);

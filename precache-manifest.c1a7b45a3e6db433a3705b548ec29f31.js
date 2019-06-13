1b1430ce3b3b5b2a26c932636c7f0e7e65311665
ali <46577578+alinaeemaei@users.noreply.github.com>
2019-06-13 12:21:25 +0430

    Updates

--- precache-manifest.c1a7b45a3e6db433a3705b548ec29f31.js
+++ precache-manifest.c1a7b45a3e6db433a3705b548ec29f31.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b207aa2d1fd2d943551b959b5da28f37",
+    "revision": "9f4a4db6cc1a26bb34bdc49fa015ec2f",
     "url": "/Final-weather/index.html"
   },
   {
-    "revision": "064ad0e30f0e7a354184",
-    "url": "/Final-weather/static/css/main.84405cab.chunk.css"
+    "revision": "c131850abcf5d3781e18",
+    "url": "/Final-weather/static/css/main.de068827.chunk.css"
   },
   {
     "revision": "a4fe92ae2dc5b87f2ca6",
     "url": "/Final-weather/static/js/2.e15fc79f.chunk.js"
   },
   {
-    "revision": "064ad0e30f0e7a354184",
+    "revision": "c131850abcf5d3781e18",
     "url": "/Final-weather/static/js/main.e36eb60f.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/Final-weather/precache-manifest.306e562e23bf65871afebac4a057243c.js"
+  "/Final-weather/precache-manifest.c1a7b45a3e6db433a3705b548ec29f31.js"
 );
 
 self.addEventListener('message', (event) => {
